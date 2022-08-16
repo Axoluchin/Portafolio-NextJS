354e1d7fbcead2222138281c5cac3aa871b16a25
Damian Huchin <huchin.h.damian@gmail.com>
2022-08-16 14:41:59 -0500

    Fix Firebase

--- pages/Portafolio.tsx
+++ pages/Portafolio.tsx
@@ -1,27 +1,34 @@
-import { Text } from '@nextui-org/react'
+import { useEffect, useState } from 'react'
+
+import useIsMounted from '../hooks/useIsMounted'
 import CardProject from '../components/CardProject'
-
 import SubTitle from '../components/SubTitle'
-
+import { getProjects } from '../utils/firebase'
 import { project } from '../utils/types'
-const example:project = {
-    date: '2022',
-    desc: 'Proyecto chido :D',
-    id: '76876873',
-    image: 'uwu',
-    name: 'Orogeddon',
-    resume: 'menos texto',
-    tech: [
-        'ts', 'js', 'firebase', 'rn'
-    ],
-    githubRepo: 'lol'
-}
 
-const Portafolio = () => (
- <div>
-  <SubTitle text='Experiencia'/>
-    <CardProject project={example}/>
- </div>
-)
+const Portafolio = () => {
+ const IsMounted = useIsMounted()
+ const [projectList, setProjectList] = useState<project[]>([])
+
+ useEffect(() => {
+  const getDBData = async () => {
+   setProjectList(await getProjects())
+  }
+  getDBData()
+ }, [])
+
+ if (!IsMounted) {
+  return null
+ }
+
+ return (
+  <div>
+   <SubTitle text="Experiencia" />
+   {projectList.map(data => (
+    <CardProject key={data.id} project={data} />
+   ))}
+  </div>
+ )
+}
 
 export default Portafolio
--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,13 +1,34 @@
 import { initializeApp } from 'firebase/app'
+import { getFirestore, getDocs, collection } from 'firebase/firestore'
 
-export const firebaseConfig = {
- apiKey: process.env.FB_apiKey,
- authDomain: process.env.FB_authDomain,
- projectId: process.env.FB_projectId,
- storageBucket: process.env.FB_storageBucket,
- messagingSenderId: process.env.FB_messagingSenderId,
- appId: process.env.FB_appId,
+import { project } from './types'
+
+const firebaseConfig = {
+ apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+ authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+ projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+ storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+ messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+ appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 }
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+const CollectionProjects = 'projects'
+
+export const getProjects = async () => {
+ const querySnapshot = await getDocs(collection(db, CollectionProjects))
+ const projectList: project[] = []
+ querySnapshot.forEach(doc => {
+  console.log(doc.id, ' => ', doc.data())
+  const project = {
+   ...doc.data(),
+   id: doc.id,
+  } as project
+  projectList.push(project)
+ })
+
+ return projectList
+}
